d5d0cd9a5a43c31767a4cecc980a878411b65118
myq1991 <myq1991@gmail.com>
2024-08-04 18:14:24 +0800

    feat: Add ExampleController and TestOptions
    Add a new file `ExampleController.ts` in the `controllers` folder. This file contains the implementation of the `ExampleController` class, which extends the `Controller` class from the `lakutata/com/entrypoint` module. The `ExampleController` class has a single method `test` that takes an input of type `ActionPattern<TestOptions>` and returns a `Promise<number>`. This method is decorated with `CLIAction`, `HTTPAction`, and `ServiceAction` decorators.
    
    Add a new file `TestOptions.ts` in the `options` folder. This file contains the implementation of the `TestOptions` class, which extends the `DTO` class from the `lakutata` module. The `TestOptions` class has a single property `timestamp` of type `number`, which is decorated with `Expect` decorator.
    
    Update the `Config.ts` file to import and include the `ExampleController` class in the `controllers` array of the `BuildEntrypoints` function.
    
    These changes add the `ExampleController` and `TestOptions` classes to the project and configure the `Config` function to include the `ExampleController` in the application options.
    
    Note: The import statement for the `ExampleController` class in the `Config.ts` file was commented out before this change.

--- packages/app/src/options/TestOptions.ts
+++ packages/app/src/options/TestOptions.ts
@@ -2,6 +2,6 @@ import {DTO, Time} from 'lakutata'
 import {Expect} from 'lakutata/decorator/dto'
 
 export class TestOptions extends DTO {
-    @Expect(DTO.Number().optional().default(() => Time.now()))
+    @Expect(DTO.Number().optional().default(() => Time.now()).description('timestamp'))
     public timestamp: number
 }
